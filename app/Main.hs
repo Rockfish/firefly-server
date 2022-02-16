8f07403457b3db417fbb4a97a6f8385d45acc05b
John Hatch <john.hatch@oracle.com>
2022-02-16 14:52:55 -0500

    added lift

--- app/Main.hs
+++ app/Main.hs
@@ -1,62 +1,65 @@
 {-# language OverloadedStrings #-}
 
 import Web.Firefly
+import Web.Firefly.Handler
+import Web.Firefly.Types
+import Data.ByteString.Lazy
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
-import Network.HTTP.Client
-import Lib
+import Network.HTTP.Client as C
+import Control.Monad.Reader
 
+import Lib
 
 -- movie data server = "www.omdbapi.com"
 -- key: 73c2e326
--- 
---main :: IO ()
---main = run 3000 app
+
+main :: IO ()
+main = run 3000 app
 
 app :: App ()
 app = do
   route "/hello" helloHandler
---  route "/movie" getMovieHandler
+  route "/movie" getMovieHandler
 
 -- | Get the 'name' query param from the url, if it doesn't exist use 'Stranger'
 helloHandler :: Handler T.Text
 helloHandler = do
   name <- fromMaybe "Stranger" <$> getQuery "name"
   return $ "Hello " `T.append` name
-  
+
+--  
+-- type MonadTrans :: ((* -> *) -> * -> *) -> Constraint
+-- class MonadTrans t where
+--   lift :: Monad m => m a -> t m a
+--   -- Defined in ‘Control.Monad.Trans.Class’
+
 getMovieHandler :: Handler T.Text
 getMovieHandler = do
-  text <- getDataTest
-  return $ "response: " `T.append` text
+  title <- fromMaybe "" <$> getQuery "title"
+  let str = show (T.unpack title)
+  text <- lift (getDataTest str)
+  return ("response: " `T.append` text)
 
---main :: IO ()
---main = getDataTest
-
-getDataTest = do
-  body <- getData
+getDataTest :: String -> IO T.Text
+getDataTest title = do
+  response <- getResponse title
+  let body = responseBody response 
   let text = T.pack $ show body
-  print text
   return text
 
-getData = do
+getUrl :: String -> String
+getUrl title = 
+    let key = "73c2e326"
+        responseType = "JSON"
+    in "http://www.omdbapi.com/?apikey=" ++ key ++ "&r=" ++ responseType ++ "&t=" ++ title
+
+getResponse :: String -> IO (Response ByteString)
+getResponse title = do
     let settings = managerSetProxy
             (proxyEnvironment Nothing)
             defaultManagerSettings
     man <- newManager settings
-    let key = "73c2e326"
-        title = "The Matrix"
-        responseType = "JSON"
-        url = "http://www.omdbapi.com/?apikey=" ++ key ++ "&r=" ++ responseType ++ "&t=" ++ title
-    request <- parseRequest $ "GET " ++ url
-    let req = "http://www.omdbapi.com/?apikey=73c2e326&r=JSON&t=The Matrix"
-            -- Note that the following settings will be completely ignored.
-            { proxy = Just $ Proxy "localhost" 1234 }
---    print url
---    print req
---    print request
-    response <- httpLbs request man
-    let body = responseBody response 
---    httpLbs req man >>= print
---    print body
-    return body
+    request <- parseRequest $ "GET " ++ getUrl title
+    httpLbs request man
 
